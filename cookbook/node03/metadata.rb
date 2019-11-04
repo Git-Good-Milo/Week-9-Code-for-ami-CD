1a57460025aa5cc4a3e7435db9517dd4ff99c5b0
Miles Eastwood <MEastwood@spartaglobal.com>
2019-11-04 15:00:32 +0000

    Inital commit of working code for ami and CD

--- cookbook/node03/metadata.rb
+++ cookbook/node03/metadata.rb
@@ -2,22 +2,24 @@ name 'node03'
 maintainer 'The Authors'
 maintainer_email 'you@example.com'
 license 'All Rights Reserved'
-description 'Installs/Configures node'
-long_description 'Installs/Configures node'
+description 'Installs/Configures node03'
+long_description 'Installs/Configures node03'
 version '0.1.0'
-chef_version '>= 12.14' if respond_to?(:chef_version)
+chef_version '>= 14.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/node/issues'
+# issues_url 'https://github.com/<insert_org_here>/node03/issues'
 
 # The `source_url` points to the development repository for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/node'
+# source_url 'https://github.com/<insert_org_here>/node03'
+depends 'nodejs', '~> 6.0.0'
+depends 'apt'
 
-
-depends 'nodejs', '~> 5.0.0'
+# Install mongodb
+# depends 'mongodb', '~> 0.16.2'
